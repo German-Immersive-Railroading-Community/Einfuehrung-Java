f0a45859e900fd1ae65d999a20970ab47268b42b
Nico Froehlich <nicofroehlich@icloud.com>
2022-09-25 00:36:20 +0200

    feat: added uebung 1

---
+++ src/uebungen/Uebung1.java
@@ -0,0 +1,50 @@
+
+/*
+ * Uebung zu schleifen und rekursion
+ * 
+ * Punkte Gesamt: 18
+ * Abgabe spaetestens: 02.10.2022 (Dann werden Loesungen veroeffentlicht)
+ */
+public class Uebung1 {
+	
+	// Punkte 4
+	public static void leicht_schleifen() {
+		/**
+		 * Gebe alle Zahlen von 1 bis 100 aus. 
+		 * 
+		 * Das print statement darf nur einmal verwendet werden. 
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+	}
+	
+	// Punkte 6
+	public static void mittel_schleifen() {
+		/**
+		 * Gebe alle Zahlen von 1 bis 100 aus, die nicht durch 3 oder 5 Teilbar sind.
+		 * Ist die Zahl durch 3 Teilbar so wird Fizz ausgegeben.
+		 * Ist die Zahl durch 5 Teilbar so wird Buzz ausgegeben.
+		 * Ist die Zahl sowohl durch durch 3 als auch durch 5 Teilbar gebe FizzBuzz aus.
+		 * Alle andere Zahlen werden normal ausgegeben
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+	}
+	
+	// Punkte 8
+	public static void schwer_rekursion(int i, int x) {
+		/**
+		 * Gebe alle Zahlen von 1 bis x aus, die nicht durch 2 Teilbar sind.
+		 * 
+		 * Es darf KEINE Schleife verwendet werden.
+		 * 
+		 * Tipp: Erstelle eine Methode mit der definition:
+		 * 
+		 * `public static void schwer_rekursion(int i, int x) {}`
+		 * 
+		 * Tipp: Zum Testen kannst du `schwer_rekursion(0, 100);` aufrufen
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+	}
+}
