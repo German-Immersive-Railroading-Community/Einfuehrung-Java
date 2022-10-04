8c59aa1d0ee961b03bf25369a7d98c148cb49413
Nico Froehlich <nicofroehlich@icloud.com>
2022-10-04 21:32:33 +0200

    feat: loesungen 1 und neue aufgaben

--- src/uebungen/Uebung1.java
+++ src/uebungen/Uebung1.java
@@ -1,54 +1,66 @@
+package uebungen;
 
 /*
- * Uebung zu schleifen und rekursion
- * Abgabe per Discord in meine DMs
+ * Uebung zu schleifen und rekursion Abgabe per Discord in meine DMs
  * 
- * Punkte Gesamt: 18
- * Abgabe spaetestens: 02.10.2022 (Dann werden Loesungen veroeffentlicht)
+ * Punkte Gesamt: 18 Abgabe spaetestens: 02.10.2022 (Dann werden Loesungen veroeffentlicht)
  */
 public class Uebung1 {
 	
 	// Punkte 4
 	public static void leicht_schleifen() {
 		/**
-		 * Gebe alle Zahlen von 1 bis 100 aus. 
-		 * 
-		 * Das print statement darf nur einmal verwendet werden.
-		 * 
-		 * EDIT: Die Ergebnisse duerfen nicht in eine String hardcoded werden
-		 * EDIT: Es muss eine Schleife sinvoll verwendet werden
-		 * 
-		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 * Gebe alle Zahlen von 1 bis 100 aus. Das print statement darf nur einmal verwendet werden. EDIT: Die Ergebnisse
+		 * duerfen nicht in eine String hardcoded werden EDIT: Es muss eine Schleife sinvoll verwendet werden Es duerfen nur
+		 * eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
 		 */
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
 	}
 	
 	// Punkte 6
 	public static void mittel_schleifen() {
 		/**
-		 * Gebe alle Zahlen von 1 bis 100 aus, die nicht durch 3 oder 5 Teilbar sind.
-		 * Ist die Zahl durch 3 Teilbar so wird Fizz ausgegeben.
-		 * Ist die Zahl durch 5 Teilbar so wird Buzz ausgegeben.
-		 * Ist die Zahl sowohl durch durch 3 als auch durch 5 Teilbar gebe FizzBuzz aus.
-		 * Alle andere Zahlen werden normal ausgegeben
-		 * 
-		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 * Gebe alle Zahlen von 1 bis 100 aus, die nicht durch 3 oder 5 Teilbar sind. Ist die Zahl durch 3 Teilbar so wird Fizz
+		 * ausgegeben. Ist die Zahl durch 5 Teilbar so wird Buzz ausgegeben. Ist die Zahl sowohl durch durch 3 als auch durch 5
+		 * Teilbar gebe FizzBuzz aus. Alle andere Zahlen werden normal ausgegeben Es duerfen nur eingefuehrte Operationen
+		 * verwendet werden. (Siehe Paket Einfuehrung)
 		 */
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0) {
+				System.out.print("Fizz");
+			}
+			if (i % 5 == 0) {
+				System.out.print("Buzz");
+			}
+			if (i % 3 != 0 && i % 5 != 0) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
 	}
 	
 	// Punkte 8
-	public static void schwer_schleifen(int x) {
+	public static void schwer_schleifen(final int x) {
 		/**
-		 * Gebe alle Primzahlen aus von 2 bis 'x'
-		 * Achte darauf das 'x' sinvoll ist
-		 * 
-		 * TIPP: Du kannst den Algo in eine seperate Methode auslagern
-		 * TIPP: Fuer X kann zum testen 100 verwendet werden.
-		 * TIPP: Es duerfen mehrere Schleifen verwendet werden
-		 * TIPP: Zum wurzel ziehen kann `Math.sqrt(x)` verwendet werden
-		 * 
-		 * Es duerfen keine Arrays verwendet werden
-		 * 
+		 * Gebe alle Primzahlen aus von 2 bis 'x' Achte darauf das 'x' sinvoll ist TIPP: Du kannst den Algo in eine seperate
+		 * Methode auslagern TIPP: Fuer X kann zum testen 100 verwendet werden. TIPP: Es duerfen mehrere Schleifen verwendet
+		 * werden TIPP: Zum wurzel ziehen kann `Math.sqrt(x)` verwendet werden Es duerfen keine Arrays verwendet werden
 		 * Darueberhinaus duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
 		 */
+		for (int i = 2; i < x; i++) {
+			boolean print = true;
+			for (int divider = 2; divider < Math.sqrt(x); divider++) {
+				if (i % divider == 0 && i != divider) {
+					print = false;
+					break;
+				}
+			}
+			if (print) {
+				System.out.println(i);
+			}
+		}
 	}
+	
 }
--- src/uebungen/Uebung2.java
+++ src/uebungen/Uebung2.java
@@ -1,21 +1,52 @@
+package uebungen;
 
-
+/*
+ * Uebung zu schleifen und rekursion Abgabe per Discord in meine DMs
+ * 
+ * Punkte Gesamt: 18
+ * 
+ * Abgabe spaetestens: 11.10.2022 (Dann werden Loesungen veroeffentlicht)
+ */
 public class Uebung2 {
 	
-	// Punkte 8
-	public static void schwer_rekursion(int i, int x) {
-		/**
-		 * Gebe alle Zahlen von 1 bis x aus, die nicht durch 2 Teilbar sind.
-		 * 
-		 * Es darf KEINE Schleife verwendet werden.
-		 * 
-		 * Tipp: Erstelle eine Methode mit der definition:
-		 * 
-		 * `public static void schwer_rekursion(int i, int x) {}`
+	// Punkte 4
+	public static void leicht_rekursion(final int i, final int x) {
+		/*
+		 * Gebe alle Zahlen von 1 bis x aus, die nicht durch 2 Teilbar sind. Es darf KEINE Schleife verwendet werden.
+		 * Tipp:Erstelle eine Methode mit der definition: `public static void schwer_rekursion(int i, int x) {}`
 		 * 
 		 * Tipp: Zum Testen kannst du `schwer_rekursion(0, 100);` aufrufen
 		 * 
 		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
 		 */
 	}
+	
+	// Punkte 6
+	public static void mittel_arrays() {
+		/*
+		 * Erstelle ein array mit den Zahlen von 1 bis 100 (automatisch befuellen)
+		 * 
+		 * Danach drehe das array um (sodass 100 vorne und 1 hinten ist) (auch automatisch)
+		 * 
+		 * Gebe das Array danach aus
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+	}
+	
+	// Punkte 8
+	public static void schwer_arrays(final int[] array) {
+		/*
+		 * Erstelle ein methode "public static void schwer_arrays(int[] array)"
+		 * 
+		 * Sortiere das array was du rein bekommst absteigend
+		 * 
+		 * Und gebe das array am ende aus
+		 * 
+		 * TIPP: Zum testen kannst du die methode mit einem test array ausfuehren
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+	}
+	
 }
