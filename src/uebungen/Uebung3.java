45ba142c8d4bd29ed6c211660e4c34058bbaba00
Nico Froehlich <nicofroehlich@icloud.com>
2022-10-12 19:42:51 +0200

    feat: add neue uebungen

---
+++ src/uebungen/Uebung3.java
@@ -0,0 +1,36 @@
+package uebungen;
+
+public class Uebung3 {
+	
+	// Punkte 4
+	public static boolean mussDasLinear(final int[] telefonBuch, final int nummer) {
+		/*
+		 * Der genial boese Dr. Meta braucht muss fuer seine boesen Plaene ganz drignend wissen Ob eine gewisse Telefon nummer
+		 * zu seinen Mitarbeitern gehoert oder nicht. Dazu hat er ein SORTIERTES Telefonbuch in Form einer Liste an Zahlen. Das
+		 * will er aber natuerlich nicht selbst machen Darum bist du als sein helfer beauftragt worden ihm dabei zu helfen!
+		 * 
+		 * Schreibe einen algorythmus der heraus findet ob die nummer im telefonBuch ist oder nicht und gebe respektive true
+		 * oder false zurueck
+		 * 
+		 * Dr. Meta moechte das moeglichst schnell hinter sich bringen.
+		 * 
+		 * Achte darauf das keine unnoetige Arbeit gemacht wird!
+		 * 
+		 * (Wer schneller als linear Zeit hinbekommt, bekommt 4 Zusatzpunkte)
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+		return false;
+	}
+	
+	// Punkte 4
+	public static int[] append(final int[] front, final int[] appende) {
+		/*
+		 * Fuege die zwei arrays so zusammen
+		 * 
+		 * Die element reihenfolge muss gleich bleiben
+		 * 
+		 * Es duerfen nur eingefuehrte Operationen verwendet werden. (Siehe Paket Einfuehrung)
+		 */
+	}
+}
