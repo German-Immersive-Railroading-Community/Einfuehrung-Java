c4aebb0eff23e4ec8775755999e4924dfc505db0
DasLichtArvid <daslicht368@gmail.com>
2022-12-19 12:41:53 +0100

    add: added new Keywords

--- src/Einfuehrung/_12_Schluesselwoerter.java
+++ src/Einfuehrung/_12_Schluesselwoerter.java
@@ -5,6 +5,9 @@ public class _12_Schluesselwoerter {
  * 	Das sind nur einige wenige Schlüsselworte, die aber wichtig sind
  * 	Schlüsselworte die wir bereits anderweitig kennengelernt haben (bspw. int oder for) sind hier auch ausgenommen
  * 
+ *  class		- erstellt eine neue Klasse
+ *  enum		- erstellt ein neues Enum
+ *  interface	- erstellt ein neues Interface
  * 	public		- die Variable, Methode oder Klasse ist im gesamten Projekt sichtbar, jeder kann darauf zugreifen
  * 	private		- die Variable oder Methode ist nur in der eigenen Klasse sichtbar, außerhalb dieser kann niemand darauf zugreifen
  * 	protected	- die Variable oder Methode ist im gesamten Projekt sichtbar, außer in nicht-erbenden Klassen aus einem anderen package
@@ -18,6 +21,12 @@ public class _12_Schluesselwoerter {
  * 	continue	- die Schleife startet einen neuen durchlauf - der nachfolgende Code innerhalb der Schleife wird in diesem Durchlauf nicht mehr ausgeführt
  * 	import		- importiert andere Klassen, so dass sie (und ihre Methoden) in dieser Klasse genutzt werden können
  * 	package		- gibt die Paketzugehörigkeit der Klasse an
+ *  new			- erzeugt ein neues Objekt (Ausnahme: Autoboxing, Autounboxing (siehe Folie 23 - Wrapper-Klassen))
+ *  this		- schafft eine Referenz innerhalb der "Bauplan"-Klasse auf das spezielle Objekt dieser Klasse
+ *  super		- schafft eine Referenz auf die direkte Superklasse: ruft bei Methodenueberschreibung die Methode der Superklasse
+ *  static		- die Methode ist nicht Objekt zugehörig und bezieht sich nur auf die Klasse: Änderungen durch diese Methode werden auf alle Objekte der Klasse angewendet
+ *  abstract	- die Klasse dient als reine "Bereitstellungsklasse" für Methoden und Attribute der Subklassen: von ihr kann kein objekt erstellt werden
+ *  extends		- die Klasse (Subklasse) erweitert eine Superklasse
  * 
  * eine vollständige Liste der Schlüsselworte gibt es hier: https://de.wikibooks.org/wiki/Java_Standard:_Schlüsselwörter
  */
