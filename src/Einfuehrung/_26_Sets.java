71d139039f3351d5569313c807310562010be3c5
DasLichtArvid <daslicht368@gmail.com>
2023-04-10 14:51:12 +0200

    fix: TreeSet ans LinkedHashset

--- src/Einfuehrung/_26_Sets.java
+++ src/Einfuehrung/_26_Sets.java
@@ -1,6 +1,7 @@
 package Einfuehrung;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class _26_Sets {
 
@@ -8,6 +9,9 @@ public class _26_Sets {
 		
 		Set<Integer> ersterSet = new HashSet<>();			// Ein Set ist eine Datenstuktur, ähnlich einer Liste, aber in einem Set tritt jedes Element nur einmal auf.
 															// Heißt: In einem Set gibt es keine doppelten Einträge.
+															// Neben "HashSet" gibt es noch "TreeSet" (dieser speichert die Eingaben gemäß ihrer Wertigkeit (Zahlen aufsteigend oder
+															// alphabetisch)) und "LinkedHashSet" (dieser Speichert die Eingaben gemäß ihrer Eingabereihenfolge), diese sind laufzeit-
+															// technisch aber langsamer als HashSets.
 		ersterSet.add(3);
 		ersterSet.add(2);
 		ersterSet.add(1);
@@ -17,7 +21,11 @@ public class _26_Sets {
 															// Achtung!: Da in einem Set die Werte nicht in der gleichen Reinfolge stehen in der sie eingegeben wurden,
 															// gibt es daher auf keine ".get()" Methode.
 															// Sämtliche anderen Methoden sind identisch zu anderen Datenstrukturen (siehe _17_Arraylisten)
-		
+		Iterator<Integer> it = ersterSet.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+/*		
 		Set<Integer> zweiterSet = Set.of(1,2,3,4);			// Neben der bekannten Erstellung eines Sets, gibt es noch eine Möglichkeit mit der Methode "Set.of()".
 															// Aber Achtung!: Dieser Set ist mit seinen Werten festgelegt (es können weder Werte hinzugefügt noch gelöscht werden).
 		
@@ -65,8 +73,8 @@ public class Waffen {
 			return false;
 		Weapon other = (Weapon) obj;
 		return Objects.equals(name, other.name);
-	}
-*/	
+*/	}
+	
 }
 
 
